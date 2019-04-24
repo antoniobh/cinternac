0e9e96c9826941a423da5347f9ff281e3ef82ef2
Antonio <g.antoniobh@gmail.com>
2019-04-24 14:42:41 -0600

    Cambios a los banners

--- src/app/core/cta/cta.component.ts
+++ src/app/core/cta/cta.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CtaComponent implements OnInit {
   @Input() Title: string;
   @Input() SubTitle: string;
+  @Input() BackgroundImage: string;
+
   constructor() { }
 
   ngOnInit() {
