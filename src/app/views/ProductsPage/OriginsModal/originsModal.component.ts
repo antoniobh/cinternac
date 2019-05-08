ad2d2d117a12245ad43cdac7def36151bada27d3
Antonio Benavides H <g.antoniobh@gmail.com>
2019-05-08 06:38:07 -0600

    Cambios_modalImagenes

--- src/app/views/ProductsPage/OriginsModal/originsModal.component.ts
+++ src/app/views/ProductsPage/OriginsModal/originsModal.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class OriginsModalComponent implements OnInit{
   @Input() Title: string;
   @Input() Identificator: string;
+  @Input() prBg: string;
   @Input() Countries:object;
 
   constructor() { }
